1b99f09994297f009427a6f9053faa463974b965
Franco Biasin <franconbiasin@gmail.com>
2021-05-20 23:23:47 -0300

    Add missing add operation to sparse numbers

--- sparse_positional_numbers.clj
+++ sparse_positional_numbers.clj
@@ -21,4 +21,14 @@
 (defn dec [n]
   (borrow 1 n))
 
+(defn add [n m]
+  (match [n m]
+    [_ ([] :seq)] n
+    [([] :seq) _] m
+    [([x & xs] :seq) ([y & ys] :seq)] (cond
+                                        (< x y) (cons x (add xs m))
+                                        (> x y) (cons y (add n ys))
+                                        (= x y) (carry (* x 2) (add xs ys)))))
+
 #_(borrow 1 (borrow 1 (borrow 1 (inc (inc (inc (inc '())))))))
+#_(add (inc (inc (inc (inc '())))) (inc (inc (inc (inc '())))))
