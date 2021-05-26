960f4136c645a236601f5f755abf8bee3eaf028d
Franco Biasin <franconbiasin@gmail.com>
2021-05-26 16:00:39 -0300

    Just give up on segmented random access lists

--- segmented_random_access_lists.clj
+++ segmented_random_access_lists.clj
@@ -41,7 +41,56 @@
 (defn c0ns [x ts]
   (c0ns- (leaf x) ts))
 
-#_(reduce #(ccc %2 %1) '() [:a :b :c :d :e :f :g :h])
+(defn ones* [n children ts]
+  (match [n ts]
+    [0 _] ts
+    [_ _] (cons [:Ones n children] ts)))
+
+(defn dec-aux [ts]
+  (match [ts]
+    [([[:Ones n children] & ts'] :seq)] [(first children) (zeros 1 (ones* (dec n) (rest children) ts'))]
+    [([[:Zeros n] & ts'] :seq)] (let [[child ts''] (dec-aux ts')]
+                                  [(leaf (nth child 1)) (ones* n (nth child 2) ts'')])))
+
+(defn dec* [ts]
+  (second (dec-aux ts)))
+
+(def a-h (reduce #(c0ns %2 %1) '() [:a :b :c :d :e :f :g :h]))
+#_(first (nth (iterate dec* a-h) 7))
+#_(dec* (dec* (reduce #(c0ns %2 %1) '() [:a :b :c :d :e :f :g :h])))
+
+(defn head [ts]
+  (second (first (dec-aux ts))))
+
+(defn log2 [x]
+  (int (Math/floor (/ (Math/log x) (Math/log 2)))))
+
+(defn exp [x n]
+  (reduce * (repeat n x)))
+
+(defn lookup-tree [[r v children] i]
+  (if (< i 1)
+    v
+    (let [idx (log2 i)]
+      (lookup-tree (nth children idx) (- i (exp 2 idx))))))
+
+(defn lookup* [ts i d]
+  (println "Hello" i)
+  (match [ts]
+    [([[:Ones n children] & ts'] :seq)] (let [to (exp 2 (+ d n))]
+                                          (if (< i to)
+                                            (let [idx (log2 (inc i))
+                                                  offset (exp 2 idx)]
+                                              (println to idx offset (count children))
+                                              (println "GOOD")
+                                              (lookup-tree (nth children idx) (- i offset)))
+                                            (lookup* ts' i (+ d n))))
+    [([[:Zeros n] & ts'] :seq)] (lookup* ts' i d)))
+
+(defn lookup [ts i]
+  (lookup* ts i 0))
+
+#_(lookup a-h 1)
 
 ;; 111 -> 0001
 ;; [[:Ones 3]] -> [[:Zeros 3] [:Ones 1]]
